ac4ae3aa37bd0d70963154444db3514f80c0c8a3
User <User@User-PC>
2014-07-28 09:35:24 +0530

    STi=udy

---
+++ AkshayServiceStudy/src/com/example/akshayservicestudy/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.akshayservicestudy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void startKaroService(View v) {
+		Intent i = new Intent(getBaseContext(), MyService.class);
+		startService(i);
+	}
+
+	public void stopKaroService(View v) {
+		Intent i = new Intent(getBaseContext(), MyService.class);
+		stopService(i);
+	}
+
+}
